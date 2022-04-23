d832ab21d68bcc14b90b358c44819e39b66a9627
Jayyp1234 <Okekejohnpaul12@gmail.com>
2022-04-23 17:38:07 +0100

    auth

--- backend/signup.php
+++ backend/signup.php
@@ -1,22 +1,27 @@
 <?php
 require('connection.php');
-
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
 
 if (isset($_POST['signup'])) {
     //List of input we would be collecting from signup form
     //All input must be required and their names must match with the html forms
-    $first_name = $con->real_escape_string($_POST['firstname']);
-    $last_name = $con->real_escape_string($_POST['lastname']);
-    $phone_number = $con->real_escape_string($_POST['phone']);
-    $country = $con->real_escape_string($_POST['country']);
-    $startup = $con->real_escape_string($_POST['startup']);
-    $gender = $con->real_escape_string($_POST['gender']);
-    $age = $con->real_escape_string($_POST['age']);
-    $category = $con->real_escape_string($_POST['category']);
-    $industry = $con->real_escape_string($_POST['industry']);
-    $stage = $con->real_escape_string($_POST['stage']);
-    $team = $con->real_escape_string($_POST['team']);
-    $startup_idea = $con->real_escape_string($_POST['idea']);
+    $first_name = $con->test_input($_POST['firstname']);
+    $last_name = $con->test_input($_POST['lastname']);
+    $phone_number = $con->test_input($_POST['phone']);
+    $country = $con->test_input($_POST['country']);
+    $startup = $con->test_input($_POST['startup']);
+    $gender = $con->test_input($_POST['gender']);
+    $age = $con->test_input($_POST['age']);
+    $category = $con->test_input($_POST['category']);
+    $industry = $con->test_input($_POST['industry']);
+    $stage = $con->test_input($_POST['stage']);
+    $team = $con->test_input($_POST['team']);
+    $startup_idea = $con->test_input($_POST['idea']);
 
 
     //SQL code to insert all our values into the table names signup
