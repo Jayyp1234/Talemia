03a9476f7d863a6e046e51cb1d9bb1395038aafb
Issac-dev <71912176+Issac-dev@users.noreply.github.com>
2022-04-27 08:25:23 +0100

    Updated subscribe.php with paystack

--- backend/subscribe.php
+++ backend/subscribe.php
@@ -13,6 +13,7 @@ if (isset($_POST['subscribe'])) {
     //List of input we would be collecting from signup form
     //All input must be required and their names must match with the html forms
     $first_name = $con->real_escape_string($_POST['firstname']);
+    $email = $con->real_escape_string($_POST['email']);
     $last_name = $con->real_escape_string($_POST['lastname']);
     $phone_number = $con->real_escape_string($_POST['phone']);
     $country = $con->real_escape_string($_POST['country']);
@@ -41,7 +42,35 @@ else {
     // include error 404 page here (page not found) here !!!
 };
     if ($signup) {
-        //redirect users to subscription page here !!!
+        $email = $con->real_escape_string($_POST['email']);
+        $price = 5000*100;
+      $url = "https://api.paystack.co/transaction/initialize";
+      $fields = [
+        'email' => $email,
+        'amount' => $price
+      ];
+      $fields_string = http_build_query($fields);
+      //open connection
+      $ch = curl_init();
+      
+      //set the url, number of POST vars, POST data
+      curl_setopt($ch,CURLOPT_URL, $url);
+      curl_setopt($ch,CURLOPT_POST, true);
+      curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        "Authorization: Bearer sk_test_ac4d89634a3d332fa722a805cff6ccce5dfb158a",
+        "Cache-Control: no-cache",
+      ));
+      
+      //So that curl_exec returns the contents of the cURL; rather than echoing it
+      curl_setopt($ch,CURLOPT_RETURNTRANSFER, true); 
+      
+      //execute post
+      $result = curl_exec($ch);
+      $myarray = json_decode($result);
+      $result2 = ($myarray->data->authorization_url);
+      // echo $result;
+      header("Location: $result2");
     } else {
         //include error 500 or 200 (Server error) here !!!
     }
