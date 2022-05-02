994ae9bab1d1fa0b01e87fa18e623b39389b7f0b
issac-dev <oladipoolayinka421@gmail.com>
2022-05-02 09:23:31 +0100

    Newsletter email validation

--- backend/newsletter.php
+++ backend/newsletter.php
@@ -14,11 +14,19 @@ if (isset($_POST['subscribe'])) {
     $last_name = $con->real_escape_string($_POST['lastname']);
     $email = $con->real_escape_string($_POST['email']);
 
-
-    //SQL code to insert all our values into the table names signup
+    if (!empty($first_name) && !empty($last_name) && !empty($email)) {
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            //SQL code to insert all our values into the table names signup
     $sql = "INSERT INTO `email` (`firstname`, `lastname`, `email`) VALUES ('$first_name','$last_name','$email')";
-     //query our SQL code
-     $subscribe = mysqli_query($con, $sql);
+    //query our SQL code
+    $subscribe = mysqli_query($con, $sql);
+            
+          } else {
+              //error page if email is not valid
+          }
+    }
+
+    
     } 
 else {
     header("Location:../index.php");
