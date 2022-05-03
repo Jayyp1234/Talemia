ab65a1ffc1cff8660f10d946104fc759b8bea57d
justusofnigeria <horlarmhedey200@gmail.com>
2022-05-03 02:34:49 +0100

    omo. i don try, subscribers.php, msg2users.php and modified a few files

--- backend/newsletter.php
+++ backend/newsletter.php
@@ -1,4 +1,9 @@
 <?php
+    include_once('../component/header1.php');
+?>
+
+
+<?php
 require('connection.php');
 
 function test_input($data) {
@@ -29,10 +34,66 @@ if (isset($_POST['subscribe'])) {
               }
         }
         else {
-            header("Location:../index.php");
+            // header("Location:../index.php");
         };
     } 
 else {
-    header("Location:../index.php");
+    // header("Location:../index.php");
 };
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Newsletter</title>
+</head>
+<body>
+    <style>
+        body{
+            justify-content: center;
+            align-items: center;
+        }
+        .box{
+            height: 80%;
+            width: 30%;
+            box-shadow: 0 0 30px -15px;
+            background-color: #fff;
+            border-radius: 10px;
+            margin: 10% auto 5%;
+            display: flex;
+            justify-content: space-evenly;
+            flex-direction: column;
+            text-align: center;
+            align-items: center;
+        }
+        .box a{
+            background-color: #09099d;
+            border-radius: 7px;
+            color: #fff;
+            font-size: 14px;
+            font-weight: 600;
+            padding:.8rem 2rem;
+            transition: .4s ease-in-out;
+        }
+        .box a:hover{
+           box-shadow: 0 0 50px -10px #09099d;
+           transform: scale(1.01);
+        }
+    </style>
+        <div class="row col-12 justify-content-center align-items-end" style="height:
+        500px">
+            <div class="box">
+                <div class="">
+                    <p><?php echo $message;?></p>
+                </div>
+                <a href="" class="btn" onclick="">Back to previous page</a>
+            </div>
+        </div>
+</body>
+</html>
+
+<?php
+    include_once('../component/footer1.php');
 ?>
