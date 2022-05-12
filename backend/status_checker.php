831bc7853428a0fadc01954d1a6b13ada09c16bf
Jayyp1234 <Okekejohnpaul12@gmail.com>
2022-05-12 15:09:54 +0100

    fixed status checker

--- backend/status_checker.php
+++ backend/status_checker.php
@@ -1,11 +1,19 @@
 <?php
   require('../backend/connection.php');
   $sql = "SELECT * FROM signup where 1 ";
+  $date = time();
   $query = mysqli_query($con,$sql);
   if($query){
     if(mysqli_num_rows($query) > 0){
       while($row = mysqli_fetch_assoc($query)){
-        
+          $email = $row['email'];
+          if ($date > $row['date_expiry']){
+            $sql = "UPDATE `signup` SET `payment_status`='notpaid' where email ='$email' ";
+            $update = mysqli_query($con, $sql);
+            if ($update){
+                echo 'changed';
+            }
+          }
       }
     }
   }
